1a72a2fa9bfcf4b6db1ef5f324c3a71c9bb1d620
Meruert <meruertmyrzabekova@gmail.com>
2025-08-22 16:46:22 +0500

    after reloading laptop

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace functions
+{
+    internal class Program
+    {
+        enum Day
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7           
+        }
+        public class Days
+        {
+
+          public static void Main(string[] args)
+            {
+                //void CompareNumbers() //TASK 1
+                //{
+                //    int a = int.Parse(Console.ReadLine());
+                //    int b = int.Parse(Console.ReadLine());
+
+                //    if (a > b)
+                //    {
+                //        Console.WriteLine(a);
+                //    }
+                //    else if (a < b)
+                //    {
+                //        Console.WriteLine(b);
+                //    }
+                //    else if (a == b)
+                //    {
+                //        Console.WriteLine("they are equal");
+                //    }
+                //}
+
+                //void DeleteFromList() //TASK2
+                //{
+                //    List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+                //    int a = 30;
+                //    numbers.Remove(a);
+                //    foreach (int n in numbers)
+                //    {
+                //        Console.WriteLine(n);
+                //    }
+
+                //}
+                //void ShowDivFive() //TASK3
+                //{
+                //    List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 };
+
+                //    foreach (int n in numbers)
+                //    {
+                //        if (n % 5 == 0)
+                //        {
+                //            Console.WriteLine(n);
+                //        }
+                //        else
+            //    {
+            //        Console.WriteLine("ololol");
+            //    }
+            //}
+
+            //}
+
+            //TASK4
+            //Console.WriteLine("enter a weekday 1-7:");
+            //int answer = int.Parse(Console.ReadLine());
+
+            //if (answer == 1)
+            //{
+            //    Day answerDay = Day.Monday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else if (answer == 2)
+            //{
+            //    Day answerDay = Day.Tuesday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else if (answer == 3)
+            //{
+            //    Day answerDay = Day.Wednesday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else if (answer == 4)
+            //{
+            //    Day answerDay = Day.Thursday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else if (answer == 5)
+            //{
+            //    Day answerDay = Day.Friday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else if (answer == 6)
+            //{
+            //    Day answerDay = Day.Saturday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else if (answer == 7)
+            //{
+            //    Day answerDay = Day.Sunday;
+            //    Console.WriteLine((int)answerDay);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not in enum");
+            //}
+        }
+    }
+    }
+   
+}
+
+
